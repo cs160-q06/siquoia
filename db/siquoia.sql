00f5e0e8d5e5e49f3f73627bd5b390d2b357f30d
joe <joe@talklikeapirateday.org>
2013-10-14 20:28:53 -0700

    Fixed syntax for tables.

--- db/siquoia.sql
+++ db/siquoia.sql
@@ -11,24 +11,35 @@ CREATE TABLE topics(
     topic VARCHAR(128) NOT NULL,
     subtopic_id INT,
     subsubtopic_id INT,
-    FOREIGN KEY (subtopic_id) REFERENCES topics (id),
-    FOREIGN KEY (subsubtopic_id) REFERENCES topics (id),
+    CONSTRAINT FOREIGN KEY (subtopic_id) REFERENCES topics (id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT FOREIGN KEY (subsubtopic_id) REFERENCES topics (id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY (id, subtopic_id, subsubtopic_id)
 );
 
 CREATE TABLE questions(
     question_id INT NOT NULL AUTO_INCREMENT,
+    topic_id INT,
     question VARCHAR(1024),
-    FOREIGN KEY (topic) REFERENCES topics (topic)
+    CONSTRAINT FOREIGN KEY (topic_id) REFERENCES topics (id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (question_id)
 );
 
 CREATE TABLE answers(
+    id INT NOT NULL AUTO_INCREMENT,
     answer VARCHAR(1024),
-    FOREIGN KEY (question_id) REFERENCES questions (question_id)
+    question_id INT,
+    CONSTRAINT FOREIGN KEY (question_id) REFERENCES questions (question_id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE scores(
-    date_taken
-    FOREIGN KEY (user_id) REFERENCES users (id)
+    date_taken TIMESTAMP(6),
+    user_id INT,
+    topic_id INT,
+    FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (topic_id) REFERENCES topics (id)
 );
