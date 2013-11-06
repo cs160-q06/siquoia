168626907a864bf22038131958af2011ec31a8ff
joe <joe@talklikeapirateday.org>
2013-11-06 04:03:42 -0800

    Split topics table.

--- db/siquoia.sql
+++ db/siquoia.sql
@@ -9,13 +9,25 @@ CREATE TABLE users(
 CREATE TABLE topics(
     id INT NOT NULL AUTO_INCREMENT,
     topic VARCHAR(128) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE subtopics(
+    id INT NOT NULL AUTO_INCREMENT,
+    subtopic VARCHAR(128) NOT NULL,
+    topic_id INT,
+    CONSTRAINT FOREIGN KEY (topic_id) REFERENCES topics (id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (id, topic_id)
+);
+
+CREATE TABLE subsubtopics(
+    id INT NOT NULL AUTO_INCREMENT,
+    subsubtopic VARCHAR(128) NOT NULL,
     subtopic_id INT,
-    subsubtopic_id INT,
-    CONSTRAINT FOREIGN KEY (subtopic_id) REFERENCES topics (id)
+    CONSTRAINT FOREIGN KEY (subtopic_id) REFERENCES subtopics (id)
         ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT FOREIGN KEY (subsubtopic_id) REFERENCES topics (id)
-        ON DELETE CASCADE ON UPDATE CASCADE,
-    PRIMARY KEY (id, subtopic_id, subsubtopic_id)
+    PRIMARY KEY (id, subtopic_id)
 );
 
 CREATE TABLE questions(
@@ -26,6 +38,8 @@ CREATE TABLE questions(
     answer1 VARCHAR(1024),
     answer2 VARCHAR(1024),
     answer3 VARCHAR(1024),
+    num_times_answered_correctly INT,
+    num_attempts INT,
     CONSTRAINT FOREIGN KEY (topic_id) REFERENCES topics (id)
         ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY (question_id)
